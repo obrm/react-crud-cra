fc8aa87ecd668de38e4e405fc48ff92bdd24bb64
Ori Baram <obrm770@gmail.com>
2023-02-22 15:08:56 +0200

    remove rating

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,7 @@
-
 import Product from './Product.component';
 import AddToCartBtn from './AddToCartBtn.component';
 import Message from './Message.component';
-import Rating from './Rating.component';
 import Header from './Header.component';
 import CartItem from './CartItem.component';
 
-export { Product, AddToCartBtn, Message, Rating, Header, CartItem };
+export { Product, AddToCartBtn, Message, Header, CartItem };
